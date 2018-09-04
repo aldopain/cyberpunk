5ea0361d022a53d99d7b941148185de2951bd486
Apple Dash <fshpsmgc@gmail.com>
2018-09-05 00:37:41 +0300

    Remade CameraController
    CameraController is now smoothly follows the mouse

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -3,17 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour {
-	GameObject player;
-	Vector3 diff;
+    public Transform FollowedObject;
+    [Range(0, 1)]
+    public float FollowSpeed;
+    public float OffsetMultiplier;
 
-    // Use this for initialization
-    void Start () {
-		player = GameObject.Find("Player");
-		diff = transform.position - player.transform.position;
-	}
-	
-	// Update is called once per frame
 	void Update () {
-		transform.position = player.transform.position + diff;
+        if(FollowedObject != null)
+        {
+            transform.position = Vector3.Lerp(transform.position, (FollowedObject.position + (Vector3.one * OffsetMultiplier) + Camera.main.ScreenToWorldPoint(Input.mousePosition))/2, FollowSpeed);
+        }
 	}
+
+    public void SetTarget(Transform t)
+    {
+        FollowedObject = t;
+    }
 }
