aaa02ca5904b1db9719cbe543a0144750147ec75
Apple Dash <fshpsmgc@gmail.com>
2018-09-05 00:59:05 +0300

    First draft of crosshair handling

---
+++ Assets/Scripts/Crosshair.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crosshair : MonoBehaviour {
+    Vector2 _mousePos;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        _mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = _mousePos;
+
+    }
+}
