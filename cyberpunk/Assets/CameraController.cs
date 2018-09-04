0f4fa8868648e7f223f22ea81255905fc69e9c44
Jaan Toming <jtoming@gmail.com>
2018-09-04 06:23:50 +0300

    made task P-0001

---
+++ cyberpunk/Assets/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+	GameObject player;
+	Vector3 diff;
+
+    // Use this for initialization
+    void Start () {
+		player = GameObject.Find("Player");
+		diff = transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = player.transform.position + diff;
+	}
+}
---
+++ cyberpunk/Assets/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+	public float speed = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void Move(){
+		var x = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+
+        transform.Translate(x, 0, z);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Move();
+	}
+}
