214aaea4d5216e139001dca612909ae9c0bce425
j.toming email = j.toming@gaijin.ru <j.toming@gaijin.lan>
2018-09-04 21:49:46 +0300

    minor fixes

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -5,14 +5,29 @@ using UnityEngine;
 public class Player : MonoBehaviour {
 	public float speed = 1.0f;
 	CharacterController cc;
+	Transform transform;
+	float originalRotation = -180;
 
 	// Use this for initialization
 	void Start () {
 		cc = GetComponent<CharacterController>();
+		transform = GetComponent<Transform>();
 	}
 
 	void Move () {
-		var move = new Vector3(Input.GetAxis("Horizontal"), -1000f, Input.GetAxis("Vertical"));
+		var h = Input.GetAxis("Horizontal");
+		var v = Input.GetAxis("Vertical");
+		var move = new Vector3(h, -1000f, v);
+		float r = 0f;
+		if (v == 0) {
+			Debug.Log("h = " + h);
+			r = 180 * (h + 1);
+			Debug.Log("r = " + r);
+		} else if (h == 0)
+			r = 90 * v;
+		else
+			r = v * (90 + 45 * h);
+		transform.rotation = Quaternion.Euler(0, r, 0);
 		cc.Move(move * Time.deltaTime * speed);
 	}
 	
