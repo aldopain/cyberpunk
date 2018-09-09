178dfc0d00adb5b900ac0fa90f2b15ceb9f9923b
Artyom <a@grebeniuk.ru>
2018-09-09 22:22:59 +0300

    Parametrizing aboveground crosshair height

--- Assets/Scripts/PlayerAiming.cs
+++ Assets/Scripts/PlayerAiming.cs
@@ -7,6 +7,7 @@ public class PlayerAiming : MonoBehaviour {
     public Crosshair _crosshair;
     public LineRenderer _line;
     public float AimingDistance;
+    public float AboveGroundCrosshairHeight = 1f;
 
     Vector3 hui = new Vector3(1,0,0);
 
@@ -29,18 +30,12 @@ public class PlayerAiming : MonoBehaviour {
         // Set the start of the line at the player position
         _line.SetPosition(0, new Vector3(transform.position.x, transform.position.y + GetComponent<CharacterController>().bounds.extents.y, transform.position.z));
 
-        
-        RaycastHit hit;
+ 
+        Vector3 hit = mousePositionToWorld();
+        Vector3 vec = Camera.main.ScreenToWorldPoint(Input.mousePosition) - hit;
+        vec = hit+ vec.normalized * AboveGroundCrosshairHeight;
 
-        //Ray ray = new Ray(transform.position, mousePositionToWorld() - transform.position);
-        Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition + hui), out hit);
-        //Physics.Raycast(ray, out hit);
-        Vector3 vec = Camera.main.ScreenToWorldPoint(Input.mousePosition) - hit.point;
-        Debug.Log(vec.normalized);
-        Debug.DrawRay(hit.point, Camera.main.ScreenToWorldPoint(Input.mousePosition), Color.green);
-        vec = hit.point+ vec.normalized;
-        _crosshair.transform.position = vec;//hit.point;
-       
+        _crosshair.transform.position = vec
         _line.SetPosition(1, _crosshair.transform.position);
     }
 }
