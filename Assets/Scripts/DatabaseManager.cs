ce50b257b4897844868da7870a88a029a1dcf8bc
j.toming email = j.toming@gaijin.ru <j.toming@gaijin.lan>
2018-09-10 18:07:21 +0300

    added database plugins and API (in progress)

---
+++ Assets/Scripts/DatabaseManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mono.Data.Sqlite;
+using System;
+using System.Data;
+
+public class DatabaseManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		string conn = "URI=file:" + Application.dataPath + "/PickAndPlaceDatabase.s3db"; //Path to database.
+		IDbConnection dbconn;
+		dbconn = (IDbConnection) new SqliteConnection(conn);
+		dbconn.Open(); //Open connection to the database.
+		IDbCommand dbcmd = dbconn.CreateCommand();
+		
+		//string sqlQuery = <SQL QUERY>;
+		//dbcmd.CommandText = sqlQuery;
+		IDataReader reader = dbcmd.ExecuteReader();
+		while (reader.Read())
+		{
+				//add here reading
+		}
+		reader.Close();
+		reader = null;
+		dbcmd.Dispose();
+		dbcmd = null;
+		dbconn.Close();
+		dbconn = null;
+	}
+}
