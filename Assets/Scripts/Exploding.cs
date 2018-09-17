e75218fc0cae7a44e277c611e785438f12b673fa
j.toming email = j.toming@gaijin.ru <j.toming@gaijin.lan>
2018-09-17 15:02:42 +0300

    TRUE ART IS... AN EXPLOSION!

---
+++ Assets/Scripts/Exploding.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Exploding : MonoBehaviour {
+    public Vector3 CenterOffset;
+    public float Radius;
+    public int Damage;
+
+    public void Explode () {
+        var Center = transform.position + CenterOffset;
+        Collider[] hitColliders = Physics.OverlapSphere (Center, Radius);
+        foreach (Collider current in hitColliders) {
+            Ray ray = new Ray (Center, current.transform.position - Center);
+            RaycastHit rh;
+
+            if (Physics.Raycast (ray, out rh, Radius) && 
+                current.GetComponent<HealthController>() != null &&
+                rh.collider.Equals (current)) {
+                    current.GetComponent<HealthController>().ChangeHealth(-Mathf.Abs(Damage));
+            }
+        }
+    }
+
+    private void OnDestroy() {
+        Explode ();
+    }
+}
