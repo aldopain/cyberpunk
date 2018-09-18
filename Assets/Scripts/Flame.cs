6d6ef854b2234ac1dd10362000ad5386f4444898
Apple Dash <fshpsmgc@gmail.com>
2018-09-18 23:45:22 +0300

    Small adjustments to flamethrower

---
+++ Assets/Scripts/Flame.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flame : MonoBehaviour {
+    public int Damage = 1;
+    public float DamageDelay = .7f;
+    public int Duration = 2;
+    private HealthController hc;
+
+    void OnEnable()
+    {
+        if(GetComponent<HealthController>() != null)
+        {
+            hc = GetComponent<HealthController>();
+            Destroy(this, Duration);
+            StartCoroutine(DealDamage());
+        }
+    }
+
+    IEnumerator DealDamage()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(DamageDelay);
+            hc.ChangeHealth(-Mathf.Abs(Damage));
+        }
+    }
+}
