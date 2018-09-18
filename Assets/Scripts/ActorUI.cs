d02dfb99c5d88cf97ec946afb74988c81149f5a5
Jaan Toming <j.toming@gaijin.lan>
2018-09-18 19:33:21 +0300

    fixed Exploding, removed prints from ActorUI

--- Assets/Scripts/ActorUI.cs
+++ Assets/Scripts/ActorUI.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.Reflection;
-using TMPro;
 
 public class ActorUI : MonoBehaviour {
     //Example of a string: Player's Max Ammo is _GenericGun.MaxMagazineCapacity_; Player's health is *HealthController.GetHealth*
@@ -10,7 +9,6 @@ public class ActorUI : MonoBehaviour {
     //public GameObject[] UsedObjects;
     public string Text;
     string formattedText;
-    public TMP_Text Output;
 	// Use this for initialization
 	void Start () {
         UpdateUI();
@@ -48,14 +46,14 @@ public class ActorUI : MonoBehaviour {
                 result = Text.Substring(first + 1, second - first - 1);
 
                 if (!result.Contains(".")){
-                    Debug.LogError("Invalid form of the string: " + result + ". It should look like <class>.<variable>");
+                    //Debug.LogError("Invalid form of the string: " + result + ". It should look like <class>.<variable>");
                 }else
                 {
                     int dotPosition = result.IndexOf(".");
                     string _class = result.Substring(0, dotPosition);
-                    print(_class);
+                    //print(_class);
                     string _var = result.Substring(dotPosition + 1, result.Length - dotPosition - 1);
-                    print(_var);
+                    //print(_var);
                     formattedText = formattedText.Replace("_" + result + "_", GetComponent(_class).GetType().GetField(_var).GetValue(gameObject.GetComponent(_class)).ToString());
                 }
             }
@@ -88,15 +86,15 @@ public class ActorUI : MonoBehaviour {
 
                 if (!result.Contains("."))
                 {
-                    Debug.LogError("Invalid form of the string: " + result + ". It should look like <class>.<function name>");
+                   // Debug.LogError("Invalid form of the string: " + result + ". It should look like <class>.<function name>");
                 }
                 else
                 {
                     int dotPosition = result.IndexOf(".");
                     string _class = result.Substring(0, dotPosition);
-                    print(_class);
+                    //print(_class);
                     string _var = result.Substring(dotPosition + 1, result.Length - dotPosition - 1);
-                    print(_var);
+                    //print(_var);
                     MethodInfo _info = GetComponent(_class).GetType().GetMethod(_var);
                     formattedText = formattedText.Replace("*" + result + "*", _info.Invoke(gameObject.GetComponent(_class), null).ToString());
                 }
@@ -104,9 +102,5 @@ public class ActorUI : MonoBehaviour {
 
             firstPass = false;
         }
-
-        Output.text = formattedText;
-
-        
     }
 }
--- Assets/Scripts/Exploding.cs
+++ Assets/Scripts/Exploding.cs
@@ -1,4 +1,6 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
 
 public class Exploding : MonoBehaviour {
     public Vector3 CenterOffset;
@@ -12,25 +14,47 @@ public class Exploding : MonoBehaviour {
         }
     }
 
+    //Sorts colliders by their distance to center of explosion
+    private List<KeyValuePair<Collider, float>> SortByDistance (Collider[] _hit) {
+        Dictionary<Collider, float> _dict = new Dictionary<Collider, float>();
+        foreach (Collider _current in _hit) {
+            _dict.Add(_current, Vector3.Distance (_current.transform.position, transform.position));
+        }
+        var _sorted = from entry in _dict orderby entry.Value ascending select entry;
+        return _sorted.ToList();
+    }
+
     public void Explode () {
         var Center = transform.position + CenterOffset;
         Collider[] hitColliders = Physics.OverlapSphere (Center, Radius);
-        foreach (Collider current in hitColliders) {
+        var CollidersAndLayers = new List<KeyValuePair<Collider, int>>();
+
+        foreach (KeyValuePair<Collider, float> _currentPair in SortByDistance(hitColliders)) {
+            Collider current = _currentPair.Key;
             Ray ray = new Ray (Center, current.transform.position - Center);
             RaycastHit rh;
 
+            //if we can raycast in collider and it has health controller
+            //then we change health, add pair Collider, Layer in list
+            //and set this collider's object's layer to 2: IgnoreRaycast
             if (Physics.Raycast (ray, out rh, Radius) && 
                 current.GetComponent<HealthController>() != null &&
                 rh.collider.Equals (current)) {
                     current.GetComponent<HealthController>().ChangeHealth(-Mathf.Abs(Damage));
+                    CollidersAndLayers.Add (new KeyValuePair<Collider, int>(current, current.gameObject.layer));
+                    current.gameObject.layer = 2;
+                    Debug.Log (current.gameObject.layer);
             }
+        }
+
+        //returning layers back
+        foreach (var _currentPair in CollidersAndLayers){
+            _currentPair.Key.gameObject.layer = _currentPair.Value;
+            Debug.Log (_currentPair.Key.gameObject.layer);
         }
     }
 
     private void OnDestroy() {
-        if (enabled)
-        {
-            Explode();
-        }      
+        Explode();
     }
 }
