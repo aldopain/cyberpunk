9a7149be20fd523a97ee440bf72ea9be25f5d53e
Jaan Toming <jtoming@gmail.com>
2018-09-19 18:55:16 +0300

    init commit

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour {
+    private GameObject _panel;
+    private bool isActive = false;
+    private float _baseTimeScale;
+
+	// Use this for initialization
+	void Start () {
+        _panel = GameObject.Find ("Panel");
+        _panel.SetActive (false);
+        _baseTimeScale = Time.timeScale;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.I)) {
+            Time.timeScale = isActive ? _baseTimeScale : 0f;
+            isActive = !isActive;
+            _panel.SetActive (isActive);
+        }
+	}
+}
