4a54b9434ea2e09cd34a9078a45781b8f0193ad3
Jaan Toming <jtoming@gmail.com>
2018-09-21 13:43:07 +0300

    added ray to vision system

--- Assets/Scripts/AI/AI_VisionSystem.cs
+++ Assets/Scripts/AI/AI_VisionSystem.cs
@@ -13,43 +13,39 @@ public class AI_VisionSystem : MonoBehaviour {
     List<GameObject> ObjectsInCone = new List<GameObject>();
     List<GameObject> ObjectsInVision = new List<GameObject>();
 
-    void Start()
-    {
+    void Start() {
         StartCoroutine(SendVision());
     }
 
-    IEnumerator SendVision()
-    {
-        while (true)
-        {
+    IEnumerator SendVision() {
+        while (true) {
             yield return new WaitForSeconds(SensorySystem.UpdateRate);
             SensorySystem.RecieveVision(ObjectsInVision);
         }
     }
 
-    //Returns true if other transform can be hit with a ray
-    bool DetectVisibility(Transform other)
-    {
-        return true;
+    //Returns true if other transform with layer Environment can be hit with a ray
+    bool DetectVisibility (Transform other) {
+        Vector3 ThisCharacterPosition = GetComponentInParent<Transform>().position;
+        RaycastHit hit;
+        Physics.Linecast (ThisCharacterPosition, other.position, out hit, (1 << 9));
+        return (hit.collider == null);
     }
 
-    void OnTriggerEnter(Collider other)
-    {
-        if (!isBlind)
-        {
+    void OnTriggerEnter(Collider other) {
+        if (!isBlind) {
             ObjectsInCone.Add(other.gameObject);
-            if (DetectVisibility(other.transform))
-            {
+            if (DetectVisibility(other.transform)) {
+                print ("I SEE U");
                 ObjectsInVision.Add(other.gameObject);
             }
         }
     }
 
-    void OnTriggerExit(Collider other)
-    {
+    void OnTriggerExit(Collider other) {
         ObjectsInCone.Remove(other.gameObject);
-        if (ObjectsInVision.Contains(other.gameObject))
-        {
+        if (ObjectsInVision.Contains(other.gameObject)) {
+            print ("I DONT SEE U");
             ObjectsInVision.Remove(other.gameObject);
         }
     }
