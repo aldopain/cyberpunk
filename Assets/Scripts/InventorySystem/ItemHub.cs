391778b5272bd7fa93057003a42f87d791e15df3
Jaan Toming <jtoming@gmail.com>
2018-10-11 22:37:37 +0300

    WIP

---
+++ Assets/Scripts/InventorySystem/ItemHub.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemHub : MonoBehaviour {
+
+    public Text name;
+    public Text description;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+    public void Show (GameObject item) {
+        //var itemInfo = item.GetComponent<Item>();
+        this.enabled = true;
+        name.text = "tttt";
+        description.text = "long text";
+
+        //name.text = itemInfo.itemName;
+        //description.text = itemInfo.itemDescription;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonUp (2)){
+            print ("HIDE");
+            gameObject.SetActive(false);
+        }
+	}
+}
