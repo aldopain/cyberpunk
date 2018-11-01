4c4528d833dd530328c3c0868a56db803139f377
Apple Dash <fshpsmgc@gmail.com>
2018-11-01 23:03:56 +0300

    Dissolve shader initial commit

---
+++ Assets/Scripts/DissolveHelper.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DissolveHelper : MonoBehaviour {
+    public float Speed;
+    float f = 0f;
+    bool isDissolving;
+    bool isSolving;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (isDissolving && f < 1f)
+        {
+            f += Speed;
+        }
+
+        if (isSolving && f > 0f)
+        {
+            f -= Speed;
+        }
+
+        SetCutout();
+    }
+
+    void SetCutout()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            if (transform.GetChild(i).GetComponent<MeshRenderer>() != null)
+                transform.GetChild(i).GetComponent<MeshRenderer>().material.SetFloat("_Cutout", f);
+        }
+    }
+
+    public void DissolveChildren()
+    {
+        isDissolving = true;
+        isSolving = false;
+    }
+
+    public void SolveChildren()
+    {
+        isDissolving = false;
+        isSolving = true;
+    }
+}
---
+++ Assets/Shaders/LevelFade.shader
@@ -0,0 +1,39 @@
+﻿Shader "Custom/LevelFade" {
+	Properties {
+		_Color ("Color", Color) = (1,1,1,1)
+		_MainTex ("Albedo", 2D) = "white" {}
+		_Pattern("DissolvePattern", 2D) = "white" {}
+		_DissolveLevel("DissolveLevel", Float) = 0.0
+		_Cutout("Circle Cutout", Float) = 0.0
+	}
+	SubShader{
+		Tags{ "RenderType" = "Opaque" }
+		Cull Off
+		CGPROGRAM
+#pragma surface surf Lambert
+
+		struct Input {
+			float2 uv_MainTex;
+			float2 uv_Pattern;
+			float3 worldPos;
+			float3 viewDir;
+			float4 screenPos;
+		};
+		sampler2D _MainTex;
+		sampler2D _Pattern;
+		float _DissolveLevel;
+		float _Cutout;
+		float _Radius;
+		float _Speed;
+
+		void surf(Input IN, inout SurfaceOutput o) {
+			fixed4 pattern = tex2D(_Pattern, IN.uv_Pattern);
+			clip(frac((IN.screenPos.y) * _DissolveLevel) - _Cutout);
+			
+			o.Albedo = tex2D(_MainTex, IN.uv_MainTex).rgb;
+		}
+
+		ENDCG
+	}
+	Fallback "Diffuse"
+}
