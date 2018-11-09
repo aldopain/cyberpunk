d2b995f8eba13b65852e50cb6e3e0c7adcd8e3b7
S1LV3Rman <38568293+S1LV3Rman@users.noreply.github.com>
2018-11-09 15:54:48 +0300

    Door script added

---
+++ Assets/Door.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Door : MonoBehaviour {
+
+    public List<string> tags;
+
+    UnityEvent OnOpen;
+    UnityEvent OnClose;
+
+    Animator animator;
+    List<Collider> colliders;
+
+	void Start () {
+        colliders = new List<Collider>();
+        OnOpen = new UnityEvent();
+        OnClose = new UnityEvent();
+        animator = GetComponent<Animator>();
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        bool validCollider = false;
+
+        foreach(var tag in tags)
+        {
+            if (other.CompareTag(tag))
+                validCollider = true;
+        }
+
+        if (validCollider)
+        {
+            colliders.Add(other);
+            OnOpen.Invoke();
+            animator.SetBool("IsClosed", false);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (colliders.Contains(other))
+            colliders.Remove(other);
+
+        if (colliders.Count == 0)
+        {
+            OnClose.Invoke();
+            animator.SetBool("IsClosed", true);
+        }
+    }
+}
